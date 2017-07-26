71fd75988622d8e574680eede43f787071a9ad01
Norman Normal <bite@me>
2017-07-26 17:27:16 -0500

    Use pre-comiled binary ffmpeg.

--- fstream_file.sh
+++ fstream_file.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 APIKEY=`cat /etc/twitchapikey`
-/var/opt/ffmpeg/ffmpeg -re -i $1 -vcodec libx264 -preset fast -crf 30 -acodec aac -framerate 24 -ab 128k -ar 44100 -strict experimental -f flv rtmp://live-dfw.twitch.tv/app/$APIKEY
+/var/opt/ffmpeg-3.3.2-armhf-32bit-static/ffmpeg -re -i $1 -vcodec libx264 -preset fast -crf 30 -acodec aac -framerate 24 -ab 128k -ar 44100 -strict experimental -f flv rtmp://live-dfw.twitch.tv/app/$APIKEY
 
 exit 0
 
