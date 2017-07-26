4afbebac01131861ef0654601d9ef4d6d28ad941
Norman Normal <bite@me>
2017-07-26 17:15:09 -0500

    Use home-rolled ffmpeg

--- fstream_file.sh
+++ fstream_file.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 APIKEY=`cat /etc/twitchapikey`
-ffmpeg -re -i $1 -vcodec libx264 -preset fast -crf 30 -acodec aac -framerate 24 -ab 128k -ar 44100 -strict experimental -f flv rtmp://live-dfw.twitch.tv/app/$APIKEY
+/var/opt/ffmpeg/ffmpeg -re -i $1 -vcodec libx264 -preset fast -crf 30 -acodec aac -framerate 24 -ab 128k -ar 44100 -strict experimental -f flv rtmp://live-dfw.twitch.tv/app/$APIKEY
 
 exit 0
 
