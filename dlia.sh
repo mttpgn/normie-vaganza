a5e3c9a892697048a7da31133adc4a4a4acde4ae
Norman Normal <bite@me>
2017-07-26 17:12:19 -0500

    More bad videos

--- dlia.sh
+++ dlia.sh
@@ -33,6 +33,8 @@ while (true); do
     echo "Banned" >> $BADSTUFF
     echo "multfilmi" >> $BADSTUFF
     echo "cartoon7" >> $BADSTUFF
+    echo "South_Park_Stop_Motion_Animation_Test__1" >> $BADSTUFF
+    echo "South_Park_Stop_Motion_Animation_Test__2" >> $BADSTUFF
     echo "Jingle_bells" >> $BADSTUFF
     echo "NoAudio" >> $BADSTUFF
     echo "PhelixSlutstopplayanimation" >> $BADSTUFF
